d41a2986a78c71f15d4cfae34d88de542c5dc002
shoptorshi <shoptorshiria78@gmail.com>
2023-11-08 18:13:35 +0600

    banner added

--- src/Page/Home.jsx
+++ src/Page/Home.jsx
@@ -2,21 +2,26 @@ import { useLoaderData, useNavigation } from "react-router-dom";
 import loading from "../assets/loding animation.json";
 import Lottie from "lottie-react";
 import FeatureCard from "../Component/FeatureCard";
+import Banner from "../SharedComponent/Banner";
 
 
 const Home = () => {
     const features = useLoaderData();
     const navigation = useNavigation();
-    if(navigation.loading === "loading"){
+    if (navigation.loading === "loading") {
         return <Lottie className="max-h-screen" animationData={loading}></Lottie>
     }
 
     return (
-        <div className="w-full max-w-[1200px] mx-auto grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 mt-10">
-            {
-                features.map(feature=><FeatureCard key={feature._id} feature={feature}></FeatureCard>)
-            }
+        <div className="w-full max-w-[1200px] mx-auto mt-10">
+            <Banner ></Banner>
+            <div className=" grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 mt-10">
+                {
+                    features.map(feature => <FeatureCard key={feature._id} feature={feature}></FeatureCard>)
+                }
+            </div>
         </div>
+
     );
 };
 
